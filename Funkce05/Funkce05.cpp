1c57bfd908bbc1c1f0d71a4f0546a0c11219dd81
dostalm.04 <dostalm.04@spst.eu>
2022-12-12 13:13:20 +0100

    Přidejte soubory projektu.

---
+++ Funkce05/Funkce05.cpp
@@ -0,0 +1,75 @@
+﻿// Funkce05.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+using namespace std;
+
+
+bool JeLiche(int n, double& ap, int& pocet)
+{
+	int soucetL = 0;
+	int pocetL = 0;
+	bool jeLiche = false;
+	if (n % 2 != 0)
+	{
+		jeLiche = true;
+
+		
+		int pocetS = 0;
+		while (n > 0)
+		{
+			int cifra = n % 10;
+			if (cifra % 2 == 1)
+			{
+				pocetL++;
+				soucetL += n;
+				
+
+			}
+			if (cifra % 2 == 0)
+			{
+				pocetS++;
+			}
+			n = n / 10;
+		
+			
+		}
+		pocet = pocetS;
+		
+	}
+	ap = (double)soucetL / pocetL;
+
+	
+	
+
+	return jeLiche;
+}
+
+
+
+int main()
+{
+	int n{0}, pocet{0};
+	double ap = 0;
+	cout << "Zadej cislo a program urci jestli je cislo liche: ";
+	cin >> n;
+
+	if (JeLiche(n, ap, pocet))
+	{
+		cout << "\ncislo je liche" << endl;
+		cout << "Aritmeticky prumer lichych cifer je " << ap << endl;
+		cout << "pocet sudych cifer je " << pocet << endl;
+		
+	}
+	else
+	{
+		cout << "\ncislo neni liche";
+	}
+
+
+	getchar();
+	return 0;
+
+ 
+}
+
